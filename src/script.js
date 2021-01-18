128b061f603de4122059722066dd2c4e56028311
Benjamin Yeung <b.yeung2018@gmail.com>
2021-01-18 23:32:42 +0800

    :white_check_mark: add orbit controls

--- src/script.js
+++ src/script.js
@@ -1,25 +1,24 @@
 import "./style.css"
 
 import * as THREE from "three"
-import gsap from "gsap"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
+// Cursor
+// const cursor = {
+//   x: 0,
+//   y: 0,
+// }
+
+// window.addEventListener("mousemove", (event) => {
+//   cursor.x = event.clientX / sizes.width - 0.5
+//   cursor.y = -(event.clientY / sizes.height - 0.5)
+// })
+
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector("canvas.webgl")
-
-// Scene
-const scene = new THREE.Scene()
-
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  wireframe: true,
-})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -27,9 +26,35 @@ const sizes = {
   height: 600,
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+)
+scene.add(mesh)
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+// Perspective camera
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
+
+// Orthographic camera (object remain same size regardless of distance / length())
+// need to multiply aspect ratio to horizontal left / right to retain scene aspect ratio
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// )
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -38,32 +63,32 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-gsap.to(mesh.position, {
-  delay: 0.5,
-  duration: 1,
-  x: 2,
-  ease: "bounce.out",
-})
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
-// Animation
-// rAf calls the function on the next frame, like stop motion
-// 60hz screens calls the function 60 times per second
-
-// getElapsedTime() counts up, starting from 0, using seconds
-// getElapsedTime() * Math.PI * 2 === one revolution per second
-// since it goes up in seconds, it means each second its going up by 1 unit
-const clock = new THREE.Clock()
+// Animate
+// const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+  //   const elapsedTime = clock.getElapsedTime()
 
-  //   mesh.position.y = Math.sin(elapsedTime) // starts at 0
-  //   mesh.position.x = Math.cos(elapsedTime) // starts at 1
+  // Update camera
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+  //   camera.position.y = cursor.y * 3
+  //   camera.lookAt(mesh.position)
 
-  camera.lookAt(mesh.position)
+  // Update objects
+  //   mesh.rotation.y = elapsedTime
 
+  controls.update()
+
+  // Render
   renderer.render(scene, camera)
-  requestAnimationFrame(tick)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
