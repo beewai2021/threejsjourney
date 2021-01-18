6466dd9c7b3564ead4bf23b1fe0ca9818d89b0d1
Benjamin Yeung <b.yeung2018@gmail.com>
2021-01-18 16:36:05 +0800

    :white_check_mark: add webpack

--- src/script.js
+++ src/script.js
@@ -1,22 +1,30 @@
+import "./style.css"
+
+import * as THREE from "three"
+
+// Scene
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Sizes
 const sizes = {
   width: 800,
   height: 600,
 }
 
+// Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-const canvas = document.querySelector("canvas.webgl")
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+  canvas: document.querySelector("canvas.webgl"),
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
