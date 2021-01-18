cf80ef112cfe12c6d8a709931bad9093a98891e7
Benjamin Yeung <b.yeung2018@gmail.com>
2021-01-18 18:05:33 +0800

    :white_check_mark: add group of 3 cubes

--- src/script.js
+++ src/script.js
@@ -2,29 +2,87 @@ import "./style.css"
 
 import * as THREE from "three"
 
+// Vector 3 methods
+// length() === length of distance between mesh position and center of scene
+// distanceTo() === distance between two Vector 3(s)
+// normalize() === set length between vector and center of scene to 1
+// console.log(
+//   `mesh's distance from the center of scene: ${mesh.position.length()}`
+// )
+// console.log(
+//   `distance between mesh and camera: ${mesh.position.distanceTo(
+//     camera.position
+//   )}`
+// )
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl")
+
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
 
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
   width: 800,
   height: 600,
 }
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true,
+  })
+)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true,
+  })
+)
+cube2.position.x = -2
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+    wireframe: true,
+  })
+)
+cube3.position.x = 2
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+camera.lookAt(group.position)
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas.webgl"),
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
