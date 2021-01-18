bcadbc670c959ffe7929a6de5ca6fedd31b25241
Benjamin Yeung <b.yeung2018@gmail.com>
2021-01-18 23:35:54 +0800

    :zap: start lesson 8

--- src/script.js
+++ src/script.js
@@ -1,18 +1,7 @@
 import "./style.css"
 
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-
-// Cursor
-// const cursor = {
-//   x: 0,
-//   y: 0,
-// }
-
-// window.addEventListener("mousemove", (event) => {
-//   cursor.x = event.clientX / sizes.width - 0.5
-//   cursor.y = -(event.clientY / sizes.height - 0.5)
-// })
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 /**
  * Base
@@ -20,68 +9,59 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 // Canvas
 const canvas = document.querySelector("canvas.webgl")
 
-// Sizes
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
 const sizes = {
   width: 800,
   height: 600,
 }
 
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
 )
-scene.add(mesh)
-
-// Camera
-// Perspective camera
-const aspectRatio = sizes.width / sizes.height
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
-
-// Orthographic camera (object remain same size regardless of distance / length())
-// need to multiply aspect ratio to horizontal left / right to retain scene aspect ratio
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100
-// )
-// camera.position.x = 2
-// camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
+camera.position.z = 3
 scene.add(camera)
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-})
-renderer.setSize(sizes.width, sizes.height)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Animate
-// const clock = new THREE.Clock()
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
 
 const tick = () => {
-  //   const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime()
 
-  // Update camera
-  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-  //   camera.position.y = cursor.y * 3
-  //   camera.lookAt(mesh.position)
-
-  // Update objects
-  //   mesh.rotation.y = elapsedTime
-
+  // Update controls
   controls.update()
 
   // Render
