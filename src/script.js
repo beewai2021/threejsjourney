6ee9cf383774f79f61b9d99ba2e1e39afde76f54
Benjamin Yeung <byeung@seekerscapital.com>
2021-01-19 19:58:25 +0800

    :white_check_mark: create custom geometry and buffergeometry

--- src/script.js
+++ src/script.js
@@ -1,45 +1,105 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import "./style.css"
+
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
+// const geometry = new THREE.Geometry()
+
+// // create 50 triangle geometries
+// for (let i = 0; i < 50; i++) {
+//   // create 3 random vertices for each triangle
+//   for (let j = 0; j < 3; j++) {
+//     geometry.vertices.push(
+//       new THREE.Vector3(
+//         (Math.random() - 0.5) * 4,
+//         (Math.random() - 0.5) * 4,
+//         (Math.random() - 0.5) * 4
+//       )
+//     )
+//   }
+
+//   const verticesIndex = i * 3
+
+// face is the shape represented by the specific vertex
+//   geometry.faces.push(
+//     new THREE.Face3(verticesIndex + 0, verticesIndex + 1, verticesIndex + 2)
+//   )
+// }
+
+// custom triangle buffer geometry (3 vertices)
+const geometry = new THREE.BufferGeometry()
+// const positionsArray = new Float32Array([
+//   0,
+//   0,
+//   0, // x
+//   0,
+//   1,
+//   0, // y
+//   1,
+//   0,
+//   0, // z
+// ])
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3) // each geometry has 3 vertices, x/y/z -> x/y/z -> x/y/z in the Float32Array
+// geometry.setAttribute("position", positionsAttribute)
+
+const count = 25
+
+// 50 triangles, each triangle has 3 vertices, each vertex is made up by 3 values (x,y,z)
+const positionsArray = new Float32Array(count * 3 * 3)
+// for every x/y/z value, set it to random between -0.5 and 0.5 to create random vertices
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = Math.random() - 0.5
+}
+const positionAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute("position", positionAttribute)
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.z = 3
 scene.add(camera)
 
@@ -49,7 +109,7 @@ controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -57,18 +117,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // Animate
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
