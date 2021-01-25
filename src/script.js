2cb6b0844386d53f40ebaaa8534eb187ded1f736
Benjamin Yeung <byeung@seekerscapital.com>
2021-01-25 17:57:24 +0800

    :white_check_mark: finish lesson 17

--- src/script.js
+++ src/script.js
@@ -15,60 +15,65 @@ const canvas = document.querySelector("canvas.webgl")
 // Scene
 const scene = new THREE.Scene()
 
-const axesHelper = new THREE.AxesHelper(2)
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper(2)
+// scene.add(axesHelper)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const heart = textureLoader.load("/textures/particles/10.png")
+const matcapTexture = textureLoader.load("/textures/matcap.png")
 
 // Particle
 // particle geometry
-const customBufferGeometry = new THREE.BufferGeometry()
-// s-shaped 2d geometry
-const vertices = new Float32Array([
-  -1.0,
-  1.0,
-  0.0, // top triangle top left
-  1.0,
-  1.0,
-  0.0, // top triangle top right
-  -1.0,
-  0.5,
-  0.0, // top triangle bottom left
-  -1.0,
-  0.5,
-  0.0, // 2nd triangle top left
-  1.0,
-  0.5,
-  0.0, // 2nd triangle top right
-  1.0,
-  0.0,
-  0.0, // 2nd triangle bottom right
-  1.0,
-  0.0,
-  0.0, // 3rd triangle top right
-  -1.0,
-  0.0,
-  0.0, // 3rd triangle top left
-  -1.0,
-  -0.5,
-  0.0, // 3rd triangle bottom left
-])
-const attributes = new THREE.BufferAttribute(vertices, 3)
-customBufferGeometry.setAttribute("position", attributes)
+const particlesGeometry = new THREE.BufferGeometry()
+
+const verticesCount = 5000
+
+const positions = new Float32Array(verticesCount * 3) // positions attribute (x,y,z)
+const colors = new Float32Array(verticesCount * 3) // colors attribute (r,g,b)
+
+for (let i = 0; i < verticesCount * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10 // -5 <---> 5
+  colors[i] = Math.random() // -1 <---> 1
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+)
+
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3))
 
 // particle material
 const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.02, // size of particle,
+  size: 0.1, // size of particle,
   sizeAttenuation: true, // particle sizing relative to camera (far - small, near - big)
+  // color: new THREE.Color(0xff000ff),
+  vertexColors: true, // use 'color' attribute stored inside geometry.attributes.color
+  // map: heart,
+  transparent: true,
+  alphaMap: heart, // render visibility based on white and black
+  // alphaTest: 0.01, // 0 <---> 1 determine how white the pixel has to be to be rendered, 0 - everything will render, 1 - nothing will be rendered
+  // depthTest: false, // turn depth test off (objects will phase / clip through each other)
+  depthWrite: false, // leave depth test on, but dont draw inside depth buffer
+  blending: THREE.AdditiveBlending, // like photoshop Overlay blend mode, will impact performance
 })
 
-// points
-const particles = new THREE.Points(customBufferGeometry, particlesMaterial)
+// particles
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 
 scene.add(particles)
+
+gui.add(particles.material, "size", 0, 1, 0.001)
+
+const cube = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(),
+  new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+)
+
+scene.add(cube)
 
 /**
  * Sizes
@@ -123,8 +128,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+// to animate each particle (meaning each vertex's x/y/z of a geometry's 'position' attribute, its best performance wise to use custom shaders)
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  particles.rotation.y = elapsedTime / 10
 
   // Update controls
   controls.update()
