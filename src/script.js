f6e42716f7abe9ef29e3e1c3e0f066e3572cb661
Benjamin Yeung <byeung@seekerscapital.com>
2021-02-04 09:52:50 +0800

    :zap: start of lesson 22

--- src/script.js
+++ src/script.js
@@ -1,10 +1,7 @@
+import "./style.css"
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
 import * as dat from "dat.gui"
-
-import "./style.css"
 
 /**
  * Base
@@ -19,51 +16,13 @@ const canvas = document.querySelector("canvas.webgl")
 const scene = new THREE.Scene()
 
 /**
- * Models
+ * Test sphere
  */
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath("/draco/")
-
-const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(dracoLoader)
-
-let mixer = null
-
-gltfLoader.load("/models/hamburger.glb", (gltf) => {
-  scene.add(gltf.scene)
-})
-
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(50, 50),
-  new THREE.MeshStandardMaterial({
-    color: "#444444",
-    metalness: 0,
-    roughness: 0.5,
-  })
+const testSphere = new THREE.Mesh(
+  new THREE.SphereGeometry(1, 32, 32),
+  new THREE.MeshBasicMaterial()
 )
-floor.receiveShadow = true
-floor.rotation.x = -Math.PI * 0.5
-scene.add(floor)
-
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = -7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = -7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+scene.add(testSphere)
 
 /**
  * Sizes
@@ -97,12 +56,11 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 )
-camera.position.set(-8, 4, 8)
+camera.position.set(4, 1, -4)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 1, 0)
 controls.enableDamping = true
 
 /**
@@ -111,26 +69,13 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-let previousTime = 0
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
-  const deltaTime = elapsedTime - previousTime
-  previousTime = elapsedTime
-
-  if (mixer) {
-    mixer.update(deltaTime)
-  }
-
   // Update controls
   controls.update()
 
