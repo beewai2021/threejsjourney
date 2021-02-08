324955a8816dbec3e5efe38e7a6dde4ee89cc8cc
Benjamin Yeung <byeung@seekerscapital.com>
2021-02-08 15:10:36 +0800

    :zap: add post processing, up until custom shader passes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 import * as THREE from "three"
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass"
+import { DotScreenPass } from "three/examples/jsm/postprocessing/DotScreenPass"
+import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass"
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass"
+import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass"
+import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import * as dat from "dat.gui"
@@ -99,6 +107,10 @@ window.addEventListener("resize", () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  // update effect composer
+  effectComposer.setSize(sizes.width, sizes.height)
+  effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -117,6 +129,7 @@ scene.add(camera)
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+controls.autoRotate = true
 
 /**
  * Renderer
@@ -134,6 +147,80 @@ renderer.toneMappingExposure = 1.5
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+let RenderTargetClass = null
+const rendererPixelRatio = renderer.getPixelRatio()
+const webGL2available = renderer.capabilities.isWebGL2
+// use built in webGL 2 anti alias if pixel ratio is lower than 2 and webGL 2 is available
+if (rendererPixelRatio < 2 && webGL2available) {
+  RenderTargetClass = THREE.WebGLMultisampleRenderTarget
+} else {
+  // otherwise use default render target and use SMAA pass
+  RenderTargetClass = THREE.WebGLRenderTarget
+}
+
+// Post processing
+// multisample render target supports MSAA (but not available in some browsers, 76% support)
+const customRenderTarget = new RenderTargetClass(
+  // size does not matter, as it will be handled by effect composer
+  100, // height
+  100, // width
+  {
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    format: THREE.RGBAFormat,
+    encoding: THREE.sRGBEncoding, // fix three.js effect composer render target output encoding when having multiple passes
+  }
+)
+
+// effect composer is the composition of all render passes
+const effectComposer = new EffectComposer(renderer, customRenderTarget)
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+effectComposer.setSize(sizes.width, sizes.height)
+
+// first render pass
+// if only one pass exists, it wont go into a render target
+// when using multiple passes (or multiple render targets), then the default renderer output encoding uses the default value of LinearEncoding
+// multiple passes will disable anti alias, as it goes to the render target
+const renderPass = new RenderPass(scene, camera) // first pass targets the existing scene
+effectComposer.addPass(renderPass)
+
+const dotScreenPass = new DotScreenPass()
+dotScreenPass.enabled = false // toggle pass
+effectComposer.addPass(dotScreenPass)
+
+const glitchPass = new GlitchPass()
+glitchPass.enabled = false
+glitchPass.goWild = false
+effectComposer.addPass(glitchPass)
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader)
+rgbShiftPass.enabled = false
+effectComposer.addPass(rgbShiftPass)
+
+const unrealBloomPass = new UnrealBloomPass()
+unrealBloomPass.enabled = true
+unrealBloomPass.strength = 0.646
+unrealBloomPass.threshold = 0.668
+unrealBloomPass.radius = 1.362
+// 3 main parameters
+// strength = intensity of bloom
+// threshold = when elements start to glow
+// radius = how far the glow extends
+// gui.add(unrealBloomPass, "strength", 0, 2, 0.001)
+// gui.add(unrealBloomPass, "threshold", 0, 2, 0.001)
+// gui.add(unrealBloomPass, "radius", 0, 2, 0.001)
+gui.add(unrealBloomPass, "enabled")
+effectComposer.addPass(unrealBloomPass)
+
+// smaa has to be at the last pass in order
+// cost to performance
+// use smaa pass as a last resort
+if (rendererPixelRatio < 2 && !webGL2available) {
+  const smaaPass = new SMAAPass()
+  smaaPass.enabled = true
+  effectComposer.addPass(smaaPass)
+}
+
 /**
  * Animate
  */
@@ -146,7 +233,8 @@ const tick = () => {
   controls.update()
 
   // Render
-  renderer.render(scene, camera)
+  //   renderer.render(scene, camera)
+  effectComposer.render()
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
