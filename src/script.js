3d9ac3ae21ac355c513efb2565555bb17dc5664b
Benjamin Yeung <byeung@seekerscapital.com>
2021-02-09 17:35:51 +0800

    :zap: start of lesson 32

--- src/script.js
+++ src/script.js
@@ -1,23 +1,47 @@
 import * as THREE from "three"
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass"
-import { DotScreenPass } from "three/examples/jsm/postprocessing/DotScreenPass"
-import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass"
-import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
-import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass"
-import { SMAAPass } from "three/examples/jsm/postprocessing/SMAAPass"
-import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
-import * as dat from "dat.gui"
+import { gsap } from "gsap"
 
 import "./style.css"
+
+/**
+ * Loaders
+ */
+const loadingBarElement = document.querySelector(".loading-bar")
+const loadingManager = new THREE.LoadingManager(
+  // Loaded
+  () => {
+    // Wait a little
+    window.setTimeout(() => {
+      // Animate overlay
+      gsap.to(overlayMaterial.uniforms.uAlpha, {
+        duration: 3,
+        value: 0,
+        delay: 1,
+      })
+
+      // Update loadingBarElement
+      loadingBarElement.classList.add("ended")
+      loadingBarElement.style.transform = ""
+    }, 500)
+  },
+
+  // Progress
+  (itemUrl, itemsLoaded, itemsTotal) => {
+    // Calculate the progress and update the loadingBarElement
+    const progressRatio = itemsLoaded / itemsTotal
+    loadingBarElement.style.transform = `scaleX(${progressRatio})`
+  }
+)
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl")
@@ -26,11 +50,32 @@ const canvas = document.querySelector("canvas.webgl")
 const scene = new THREE.Scene()
 
 /**
- * Loaders
+ * Overlay
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
-const textureLoader = new THREE.TextureLoader()
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+  // wireframe: true,
+  transparent: true,
+  uniforms: {
+    uAlpha: { value: 1 },
+  },
+  vertexShader: `
+        void main()
+        {
+            gl_Position = vec4(position, 1.0);
+        }
+    `,
+  fragmentShader: `
+        uniform float uAlpha;
+
+        void main()
+        {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `,
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
 
 /**
  * Update all materials
@@ -41,7 +86,8 @@ const updateAllMaterials = () => {
       child instanceof THREE.Mesh &&
       child.material instanceof THREE.MeshStandardMaterial
     ) {
-      child.material.envMapIntensity = 5
+      // child.material.envMap = environmentMap
+      child.material.envMapIntensity = debugObject.envMapIntensity
       child.material.needsUpdate = true
       child.castShadow = true
       child.receiveShadow = true
@@ -60,16 +106,19 @@ const environmentMap = cubeTextureLoader.load([
   "/textures/environmentMaps/0/pz.jpg",
   "/textures/environmentMaps/0/nz.jpg",
 ])
+
 environmentMap.encoding = THREE.sRGBEncoding
 
 scene.background = environmentMap
 scene.environment = environmentMap
 
+debugObject.envMapIntensity = 5
+
 /**
  * Models
  */
 gltfLoader.load("/models/DamagedHelmet/glTF/DamagedHelmet.gltf", (gltf) => {
-  gltf.scene.scale.set(2, 2, 2)
+  gltf.scene.scale.set(2.5, 2.5, 2.5)
   gltf.scene.rotation.y = Math.PI * 0.5
   scene.add(gltf.scene)
 
@@ -81,8 +130,8 @@ gltfLoader.load("/models/DamagedHelmet/glTF/DamagedHelmet.gltf", (gltf) => {
  */
 const directionalLight = new THREE.DirectionalLight("#ffffff", 3)
 directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.normalBias = 0.05
 directionalLight.position.set(0.25, 3, -2.25)
 scene.add(directionalLight)
@@ -107,10 +156,6 @@ window.addEventListener("resize", () => {
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-  // update effect composer
-  effectComposer.setSize(sizes.width, sizes.height)
-  effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -129,7 +174,6 @@ scene.add(camera)
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.autoRotate = true
 
 /**
  * Renderer
@@ -138,103 +182,24 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
 renderer.physicallyCorrectLights = true
 renderer.outputEncoding = THREE.sRGBEncoding
 renderer.toneMapping = THREE.ReinhardToneMapping
-renderer.toneMappingExposure = 1.5
+renderer.toneMappingExposure = 3
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-let RenderTargetClass = null
-const rendererPixelRatio = renderer.getPixelRatio()
-const webGL2available = renderer.capabilities.isWebGL2
-// use built in webGL 2 anti alias if pixel ratio is lower than 2 and webGL 2 is available
-if (rendererPixelRatio < 2 && webGL2available) {
-  RenderTargetClass = THREE.WebGLMultisampleRenderTarget
-} else {
-  // otherwise use default render target and use SMAA pass
-  RenderTargetClass = THREE.WebGLRenderTarget
-}
-
-// Post processing
-// multisample render target supports MSAA (but not available in some browsers, 76% support)
-const customRenderTarget = new RenderTargetClass(
-  // size does not matter, as it will be handled by effect composer
-  100, // height
-  100, // width
-  {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.LinearFilter,
-    format: THREE.RGBAFormat,
-    encoding: THREE.sRGBEncoding, // fix three.js effect composer render target output encoding when having multiple passes
-  }
-)
-
-// effect composer is the composition of all render passes
-const effectComposer = new EffectComposer(renderer, customRenderTarget)
-effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-effectComposer.setSize(sizes.width, sizes.height)
-
-// first render pass
-// if only one pass exists, it wont go into a render target
-// when using multiple passes (or multiple render targets), then the default renderer output encoding uses the default value of LinearEncoding
-// multiple passes will disable anti alias, as it goes to the render target
-const renderPass = new RenderPass(scene, camera) // first pass targets the existing scene
-effectComposer.addPass(renderPass)
-
-const dotScreenPass = new DotScreenPass()
-dotScreenPass.enabled = false // toggle pass
-effectComposer.addPass(dotScreenPass)
-
-const glitchPass = new GlitchPass()
-glitchPass.enabled = false
-glitchPass.goWild = false
-effectComposer.addPass(glitchPass)
-
-const rgbShiftPass = new ShaderPass(RGBShiftShader)
-rgbShiftPass.enabled = false
-effectComposer.addPass(rgbShiftPass)
-
-const unrealBloomPass = new UnrealBloomPass()
-unrealBloomPass.enabled = true
-unrealBloomPass.strength = 0.646
-unrealBloomPass.threshold = 0.668
-unrealBloomPass.radius = 1.362
-// 3 main parameters
-// strength = intensity of bloom
-// threshold = when elements start to glow
-// radius = how far the glow extends
-// gui.add(unrealBloomPass, "strength", 0, 2, 0.001)
-// gui.add(unrealBloomPass, "threshold", 0, 2, 0.001)
-// gui.add(unrealBloomPass, "radius", 0, 2, 0.001)
-gui.add(unrealBloomPass, "enabled")
-effectComposer.addPass(unrealBloomPass)
-
-// smaa has to be at the last pass in order
-// cost to performance
-// use smaa pass as a last resort
-if (rendererPixelRatio < 2 && !webGL2available) {
-  const smaaPass = new SMAAPass()
-  smaaPass.enabled = true
-  effectComposer.addPass(smaaPass)
-}
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
-
   // Update controls
   controls.update()
 
   // Render
-  //   renderer.render(scene, camera)
-  effectComposer.render()
+  renderer.render(scene, camera)
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
