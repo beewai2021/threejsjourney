17fc587acb754fdf948044af9f1f9b2c414a0d1d
Benjamin Yeung <byeung@seekerscapital.com>
2021-02-22 10:43:05 +0800

    :zap: refactor code and tighten up comments

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,18 +1,22 @@
 precision mediump float; // precision level of float values (lowp, mediump, highp), high precision is more performance heavy, and might not work on all devices. lowp may cause bugs based on lack of precision
 
+// custom uniforms passed from js
 uniform vec3 uColor;
-uniform sampler2D uTexture;
+uniform sampler2D uTexture; // 2D image texture. samplerCube for 2D cubemap texture
 
-// varying are interpolated (between fragments in this example)
-// varying float vRandom; // random attribute passed from vertex shader
+// passed from vertex shaders
+// varying are linearly interpolated (between fragments in this example)
+// varying float vRandom;
 varying vec2 vUv;
 varying float vElevation;
 
 void main() {
-    // rgba, pick color from texture and apply color on fragment based on uv coordinates
+    // pick color from texture and apply color onto fragments based on uv coordinates positioning
     vec4 textureColor = texture2D(uTexture, vUv);
+
+    // artificial shadows based on elevation
     textureColor.rgb *= vElevation * 2.0 + 0.65;
 
     // fragment color
-    gl_FragColor = vec4(textureColor); // random green and alpha
+    gl_FragColor = vec4(textureColor); // apply vec4 texture color as fragment color
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -3,43 +3,41 @@ uniform mat4 projectionMatrix; // clipspace coordinates (normalized), transform 
 uniform mat4 viewMatrix; // position based on camera position/fov/near/far/rotation
 uniform mat4 modelMatrix; // position based on mesh position/rotation/scale in the world world, rather than local space
 // uniform mat4 modelViewMatrix; // modelMatrix * viewMatrix
+
+// custom uniforms passed from js
 uniform vec2 uFrequency;
 uniform float uTime;
 
 attribute vec3 position; // different position for each vertex (x,y,z), at local space. Float32BufferAttributes
-attribute float aRandom; // custom created attribute, a for attributes
+
+// custom attributes from custom made geometry attributes from js
+attribute float aRandom; // custom created attributes, "a" for attributes
 attribute vec2 uv;
 
+// passing varyings to fragment shaders
 // varying float vRandom;
 varying vec2 vUv;
 varying float vElevation;
 
-// float sum(float a, float b) {
-//     return a + b;
-// }
-
-// functions that doesnt return anything, nor has any function parameters can use void
-
-void main() {
-    // vec2 (x,y)
-    // vec3 (x,y,z) or (r,g,b)
-    // vec4 (x,y,z,w) or (r,g,b,a)
-
-    // float result = sum(1.0, 2.0);
-    
+// functions that doesnt return anything, nor has any function parameters can use "void"
+// vec2 (x,y)
+// vec3 (x,y,z) or (r,g,b)
+// vec4 (x,y,z,w) or (r,g,b,a)
     // position of the vertex (vec4(x, y, z, w = 1.0)) in screen coordinates
     // w helps with perspective projection
-    // each matrix transforms position to get final clip space coordinates
-    // matrix applies to position through multiplication
-    // must be in the following order
-    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    // gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    
+// must be in the following order
+// matrix applies to position through multiplication
+// gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+// or
+// gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
-
-    vec4 modelPos = modelMatrix * vec4(position, 1.0); // world space position
+void main() {
+    vec4 modelPos = modelMatrix * vec4(position, 1.0); // transform local to world space coordinates
 
     float waveStrength = 0.08;
 
+    
     float elevation = sin(modelPos.x * uFrequency.x - uTime) * waveStrength;
     elevation += sin(modelPos.y * uFrequency.y - uTime) * waveStrength;
 
@@ -51,7 +49,7 @@ void main() {
     vElevation = elevation;
 
     vec4 viewPos = viewMatrix * modelPos; // camera projection
-    vec4 projectionPos = projectionMatrix * viewPos;
+    vec4 projectionPos = projectionMatrix * viewPos; // add perspective if using perspective camera
 
     gl_Position = projectionPos;
 }
